cbbbdbc557cb94ad277fe97ba49ee9f8ee4910ae
zetoirque <89418946+zetoirque@users.noreply.github.com>
2023-04-28 20:23:05 +0200

    Update Main.lua

--- Random/Troll/Main.lua
+++ Random/Troll/Main.lua
@@ -4,6 +4,8 @@ local Tab = Window:NewTab("Troll")
 local Section = Tab:NewSection("Size")
 local Section2 = Tab:NewSection("Mouvement")
 
+local bool = true
+
 getgenv().FlySpeed = 500;
 
 
@@ -27,7 +29,7 @@ local lastctrl = {f = 0, b = 0, l = 0, r = 0}
 local maxspeed = 400 
 local speed = 0
 
-function Fly() 
+function Fly(PStand) 
 
     plr = game.Players.LocalPlayer
     torso = plr.Character.Head  
@@ -45,7 +47,9 @@ bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
 
 repeat wait() 
 
+if PStand ~= true then
 plr.Character.Humanoid.PlatformStand = true
+end
 
 if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
 
@@ -115,115 +119,6 @@ mouse.KeyUp:connect(function(key)
     elseif key:lower() == "d" then 
 
         ctrl.r = 0 
-
-    end 
-
-end)
-
-
---FlyScript #2
-repeat wait()
-
-until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
-
-local mouse2 = game.Players.LocalPlayer:GetMouse() 
-
-repeat wait() until mouse
-
-local plr2 = game.Players.LocalPlayer
-local torso2 = plr.Character.Head 
-local flying2 = false
-local deb2 = true 
-local ctrl2 = {f = 0, b = 0, l = 0, r = 0} 
-local lastctrl2 = {f = 0, b = 0, l = 0, r = 0} 
-local maxspeed2 = 400 
-local speed2 = 0
-
-function Fly2()
-
-    plr2 = game.Players.LocalPlayer
-    torso2 = plr2.Character.Head  
-
-local bg2 = Instance.new("BodyGyro", torso) 
-
-bg2.P = 9e4 
-bg2.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
-bg2.cframe = torso.CFrame
-
-local bv2 = Instance.new("BodyVelocity", torso) 
-
-bv2.velocity = Vector3.new(0,0.1,0) 
-bv2.maxForce = Vector3.new(9e9, 9e9, 9e9) 
-
-repeat wait() 
-
-if (ctrl2.l + ctrl2.r) ~= 0 or (ctrl2.f + ctrl2.b) ~= 0 then 
-
-    bv2.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl2.f+ctrl2.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl2.l+ctrl2.r,(ctrl2.f+ctrl2.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*FlySpeed
-
-    lastctrl2 = {f2 = ctrl2.f, b2 = ctrl2.b, l2 = ctrl2.l, r2 = ctrl2.r} 
-
-elseif (ctrl2.l + ctrl2.r) == 0 and (ctrl2.f + ctrl2.b) == 0 and speed2 ~= 0 then 
-
-    bv2.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl2.f+lastctrl2.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl2.l+lastctrl2.r,(lastctrl2.f+lastctrl2.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*FlySpeed
-
-else 
-
-    bv2.velocity = Vector3.new(0,0.1,0) 
-
-end 
-
-bg2.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl2.f+ctrl2.b)*50*speed2/maxspeed2),0,0) 
-
-until not flying2
-
-ctrl2 = {f2 = 0, b2 = 0, l2 = 0, r2 = 0} 
-lastctrl2 = {f2 = 0, b2 = 0, l2 = 0, r2 = 0} 
-speed2 = 0 
-bg2:Destroy() 
-bv2:Destroy() 
-
-end 
-
-mouse2.KeyDown:connect(function(key) 
-
-    if key:lower() == "w" then 
-
-        ctrl2.f = 1 
-
-    elseif key:lower() == "s" then 
-
-        ctrl2.b = -1 
-
-    elseif key:lower() == "a" then 
-
-        ctrl2.l = -1 
-
-    elseif key:lower() == "d" then 
-
-        ctrl2.r = 1 
-
-    end 
-
-end) 
-
-mouse2.KeyUp:connect(function(key) 
-
-    if key:lower() == "w" then 
-
-        ctrl2.f = 0 
-
-    elseif key:lower() == "s" then 
-
-        ctrl2.b = 0 
-
-    elseif key:lower() == "a" then 
-
-        ctrl2.l = 0 
-
-    elseif key:lower() == "d" then 
-
-        ctrl2.r = 0 
 
     end 
 
@@ -307,14 +202,14 @@ end)
 
 Section2:NewKeybind("Fly For Vehicule", "", Enum.KeyCode.R, function()
 
-    if flying2 == true then 
+    if flying == true then 
 
-        flying2 = false
+        flying = false
 
     else 
 
-        flying2 = true 
-        Fly2() 
+        flying = true 
+        Fly(bool)
 
     end
 end)
